288af23664b7d90454d9fdb412c5ea7be245bde1
Rachit Malik <rachit@D-10-19-121-229.dhcp4.washington.edu>
2018-10-21 13:00:38 -0700

    final updates

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import { Tesseract } from "tesseract.ts";
 import myImage from "./assets/Annotation.png";
 import Landing from "./components/landing";
 import "./App.css";
-import axios from "axios";
-import Crypto from "crypto-js";
+import cheerio from "cheerio";
+import rp from "request-promise";
+import itemsArray from "./data.js";
+
 // import urijs from "uri-js";
+
+
 var amazon = require("amazon-product-api");
 var client = amazon.createClient({
   awsId: "AKIAJP2NFVTWFZYRFW7A",
@@ -22,89 +26,44 @@ class App extends Component {
     };
   }
 
-  getASIN() {
-    // var timestmpISO = new Date().toISOString();
-    // var timestmpISO2 = timestmpISO.split("T")[0];
-    // // .replace("-", "")
-    // // .replace("-", "")
-    // // .replace(":", "")
-    // // .replace(":", "")
-    // // .replace(".", "");
-    // console.log(timestmpISO);
-    // const AWSAccessKeyID = "AKIAJP2NFVTWFZYRFW7A";
-    // const key = "WPf8GG0a0PnWQBORv2K0qHD8ok/tL1r6FciXPoKP";
-    // var kDate = Crypto.HmacSHA256(timestmpISO2, "AWS4" + key);
-    // var kRegion = Crypto.HmacSHA256("us-east-1", kDate);
-    // var kService = Crypto.HmacSHA256("webservices.amazon.com", kRegion);
-    // var kSigning = Crypto.HmacSHA256("aws4_request", kService);
+ getAsin(item) {
+   // console.log(itemsArray);
+   for(let i = 0; i<itemsArray.length; i++) {
+     const temp = itemsArray[i].name.replace("↵↵","");
+      if(temp == item)
+        return itemsArray[i].ASIN;
+    return "NO";
+   }
 
-    // axios
-    //   .get(
-    //     "http://webservices.amazon.com/onca/xml?" +
-    //       "Service=AWSECommerceService" +
-    //       "&Operation=ItemSearch" +
-    //       "&ResponseGroup=Small" +
-    //       "&SearchIndex=All" +
-    //       "&Keywords=milk" +
-    //       "&AWSAccessKeyId=" +
-    //       AWSAccessKeyID +
-    //       "&Timestamp=" +
-    //       timestmpISO +
-    //       "&Signature=" +
-    //       kSigning
-    //   )
-    //   .then(res => {
-    //     console.log(res);
-    //   });
-    // https://www.npmjs.com/package/amazon-product-api
-    client
-      .itemSearch({
-        director: "Quentin Tarantino",
-        actor: "Samuel L. Jackson",
-        searchIndex: "DVD",
-        audienceRating: "R",
-        responseGroup: "ItemAttributes,Offers,Images"
-      })
-      .then(function(results) {
-        console.log(results);
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-    return "";
-  }
+ };
 
-  apendASIM(item) {
-    let tASIN = this.getASIN(item);
-    let tstr = "&ASIN.1=" + tASIN + "&Quantity.1=1";
-    return tstr;
+   apendASIM(item) {
+    return "&ASIN.1=" + this.getAsin(item) + "&Quantity.1=1";
   }
 
   componentDidMount() {
     Tesseract.recognize(myImage, {
-      lang: "eng",
-      tessedit_ambigs_training: 1
-    })
-      .then(function(result) {
-        console.log(result);
-      })
+        lang: "eng",
+        tessedit_ambigs_training: 1
+     })
+     .then(function(result) {
+        console.log(result.lines);
+     })
       .then(result => {
+        let bkLink = this.state.link;
         for (let index = 0; index < result.lines.length; index++) {
-          let bkLink = this.state.link;
+          console.log(result.lines[index].text);
           bkLink += this.apendASIM(result.lines[index].text);
-          this.setState({
-            lnik: bkLink
-          });
         }
-      });
-    this.getASIN();
+        console.log(bkLink);
+    });
   }
 
   render() {
     return (
       <Fragment>
         <div className="App">
-          {/* <Landing /> */}
+          { <Landing /> }
           <p>{this.state.link}</p>
         </div>
       </Fragment>
